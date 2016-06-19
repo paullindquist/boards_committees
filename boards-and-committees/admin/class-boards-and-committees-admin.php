4512020c8e0f37664126f645856c84174b0795f7
Paul Lindquist <paul.lindquist@gmail.com>
2016-06-19 09:24:49 -0500

    added menu item

--- boards-and-committees/admin/class-boards-and-committees-admin.php
+++ boards-and-committees/admin/class-boards-and-committees-admin.php
@@ -22,82 +22,100 @@
  */
 class Boards_And_Committees_Admin {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $boards_and_committees    The ID of this plugin.
-	 */
-	private $boards_and_committees;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $boards_and_committees    The ID of this plugin.
+     */
+    private $boards_and_committees;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $boards_and_committees       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $boards_and_committees, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $boards_and_committees       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct( $boards_and_committees, $version ) {
 
-		$this->boards_and_committees = $boards_and_committees;
-		$this->version = $version;
+        $this->boards_and_committees = $boards_and_committees;
+        $this->version = $version;
 
-	}
+    }
+    /**
+     *	Displays admin page
+     *
+     * @since    1.0.1
+     */
+    public function output_content() {
+        echo '<h2>here\'s some output<h2>';
+    }
 
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Boards_And_Committees_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Boards_And_Committees_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    /**
+     * Adds the admin menu item
+     *
+     * @since    1.0.1
+     */
+    public function add_menu_page() {
+        add_menu_page( 'Boards and Committees', 'Boards and Committees', 'edit_posts', 'boards_and_committees', array($this, 'output_content'), 'dashicons-id', 7  );
+    }
 
-		wp_enqueue_style( $this->boards_and_committees, plugin_dir_url( __FILE__ ) . 'css/boards-and-committees-admin.css', array(), $this->version, 'all' );
+    /**
+     * Register the stylesheets for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles() {
 
-	}
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Boards_And_Committees_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Boards_And_Committees_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+        wp_enqueue_style( $this->boards_and_committees, plugin_dir_url( __FILE__ ) . 'css/boards-and-committees-admin.css', array(), $this->version, 'all' );
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Boards_And_Committees_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Boards_And_Committees_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    }
 
-		wp_enqueue_script( $this->boards_and_committees, plugin_dir_url( __FILE__ ) . 'js/boards-and-committees-admin.js', array( 'jquery' ), $this->version, false );
+    /**
+     * Register the JavaScript for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts() {
 
-	}
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Boards_And_Committees_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Boards_And_Committees_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
+
+        wp_enqueue_script( $this->boards_and_committees, plugin_dir_url( __FILE__ ) . 'js/boards-and-committees-admin.js', array( 'jquery' ), $this->version, false );
+
+    }
 
 }
--- boards-and-committees/includes/class-boards-and-committees.php
+++ boards-and-committees/includes/class-boards-and-committees.php
@@ -153,6 +153,7 @@ class Boards_And_Committees {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_menu_page' );
 
 	}
 
