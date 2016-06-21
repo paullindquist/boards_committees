bada34fbaecc546a26235b9fa4339bf18a44b8b9
Paul Lindquist <paul.lindquist@gmail.com>
2016-06-21 18:08:03 -0500

    adding tables to db on activation

--- boards-and-committees/includes/class-boards-and-committees-activator.php
+++ boards-and-committees/includes/class-boards-and-committees-activator.php
@@ -23,13 +23,58 @@
 class Boards_And_Committees_Activator {
 
 	/**
-	 * Short Description. (use period)
+	 * Create boards and committees tables
 	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
+	 * @since    1.0.1
 	 */
 	public static function activate() {
+        global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$groups_table_name = $wpdb->prefix . 'groups';
+		$groups_sql = "CREATE TABLE $groups_table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			name TEXT NOT NULL,
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		$boards_table_name = $wpdb->prefix . 'boards';
+		$boards_sql = "CREATE TABLE $boards_table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+            group_type mediumint(9),
+			name TEXT NOT NULL,
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		$committees_table_name = $wpdb->prefix . 'committees';
+		$committees_sql = "CREATE TABLE $committees_table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+            group_type mediumint(9),
+			name TEXT NOT NULL,
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		$member_table_name = $wpdb->prefix . 'member';
+		$member_sql = "CREATE TABLE $member_table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			name TEXT NOT NULL,
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		$member_group_table_name = $wpdb->prefix . 'member_group';
+		$member_group_sql = "CREATE TABLE $member_group_table_name (
+			group_id mediumint(9) NOT NULL,
+			member_id mediumint(9) NOT NULL,
+			UNIQUE KEY (group_id, member_id)
+		) $charset_collate;";
+
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $groups_sql );
+		dbDelta( $boards_sql );
+		dbDelta( $committees_sql );
+		dbDelta( $member_sql );
+		dbDelta( $member_group_sql );
 
 	}
 
